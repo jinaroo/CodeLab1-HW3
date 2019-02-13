1b5b2d9b93b6b3c79441557965a31e92c51f6c75
jinaroo <sohnjin@gmail.com>
2019-02-13 12:37:30 -0500

    added week 1 hw folder to continue working on for this week

---
+++ codeLabWeek1HW/Assets/Scripts/badStarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class badStarController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<velocityPlayerController>().lives--; //lives go down
+        }
+        
+        Destroy(gameObject);
+    }
+}
